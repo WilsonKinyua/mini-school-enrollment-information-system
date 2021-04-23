fa03fa6778d2ff9c29f8856c8eb8d713315adff9
WilsonKinyua <wilsonkinyuam@gmail.com>
2021-04-24 02:54:44 +0300

    dhasboard updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,15 @@
-@extends('layouts.admin')
-@section('content')
-<div class="content">
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="card">
-                <div class="card-header">
-                    Dashboard
-                </div>
-
-                <div class="card-body">
-                    @if(session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+@extends('layouts.main')
+@section('title')
+    Dashboard
+@endsection
+@section('breadcubs')
+<div class="breadcrumbs-area">
+    <h3>Dashboard</h3>
+    <ul>
+        <li>
+            <a href="/">Home</a>
+        </li>
+    </ul>
 </div>
 @endsection
 @section('scripts')
